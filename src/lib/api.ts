795eda83ecc824538d14ca0ddc202dc20abd3cf1
Ali Abbasi <al5690410@gmail.com>
2025-07-10 01:46:38 +0500

    fix: change limits, and set cool down times to prevent race conditions.

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const API_BASE_URL = 'https://guysolan.pythonanywhere.com';
+// const API_BASE_URL = 'http://127.0.0.1:5000';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
