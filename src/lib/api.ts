3437f15e1b4906e32e9e1e7243a7ae3ad4e2992f
Ali Abbasi <al5690410@gmail.com>
2025-07-15 20:07:19 +0500

    Revert "fix: change limits, and set cool down times to prevent race conditions."
    This reverts commit 795eda83ecc824538d14ca0ddc202dc20abd3cf1.

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000';
+const API_BASE_URL = 'https://guysolan.pythonanywhere.com';
+// const API_BASE_URL = 'http://localhost:5000';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
